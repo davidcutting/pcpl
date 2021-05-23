b20b322f2f96aa7dd7579cc9d014ee8800eaf9dc
dcutting133 <dcutting@umich.edu>
2021-05-23 17:06:25 -0400

    include first revision architecture documentation and make simple ros2 node

--- README.md
+++ README.md
@@ -1,2 +1,15 @@
 # lidar_processor
-A simple PointCloud processor for ground based robots using a 3D LiDAR
+A simple PointCloud processor for ground based robots using a 3D LiDAR.
+
+## Major Components
+
+* **Passthrough Filter** - Cropping PointClouds to a range of acceptable and usable values.
+* **Voxel Filter** - Downsampling PointClouds to speed up more computationally intensive filters.
+* **Segementation** - Separating parts of PointClouds to ease in map building. Examples include: Ground Points, Obstacle Points, Overhead Points
+* **Post-Processor** - Using segemented PointCloud indicies to generate new fully filtered PointClouds, or doing a 2D projection for scan based mapping.
+
+![PointCloud Pipeline](doc/lidar_processor_architecture.png "PointCloud Pipeline")
+
+## Additional Info
+
+This can be used to fix strange ign_gazebo frame_ids which come from a URDF robot model being spawned in to the simulator with an attached gpu_ray.
--- src/lidar_processor.cpp
+++ src/lidar_processor.cpp
@@ -1,10 +1,39 @@
-#include <cstdio>
+#include <memory>
+#include <functional>
 
-int main(int argc, char ** argv)
+#include "lidar_processor/lidar_processor.h"
+
+LidarProcessor::LidarProcessor(rclcpp::NodeOptions options) : Node("lidar_processor", options)
 {
-  (void) argc;
-  (void) argv;
+  filtered_pcl_publisher_ = this->create_publisher<sensor_msgs::msg::PointCloud2>("/lidar/filtered_points", 10);
+  raw_pcl_subscription_ = this->create_subscription<sensor_msgs::msg::PointCloud2>(
+  "/lidar/raw_points", 10, std::bind(&LidarProcessor::raw_pcl_callback, this, std::placeholders::_1));
 
-  printf("hello world lidar_processor package\n");
+  filtered_ls_publisher_ = this->create_publisher<sensor_msgs::msg::LaserScan>("/lidar/filtered_scan", 10);
+  raw_ls_subscription_ = this->create_subscription<sensor_msgs::msg::LaserScan>(
+  "/lidar/raw_scan", 10, std::bind(&LidarProcessor::raw_ls_callback, this, std::placeholders::_1));
+}
+
+void LidarProcessor::raw_pcl_callback(const sensor_msgs::msg::PointCloud2::SharedPtr msg)
+{
+  msg->header.frame_id = "laser_link"; // fix weird pointcloud frame?
+  filtered_pcl_publisher_->publish(*msg);
+}
+
+void LidarProcessor::raw_ls_callback(const sensor_msgs::msg::LaserScan::SharedPtr msg)
+{
+  msg->header.frame_id = "laser_link"; // fix weird scan frame?
+  filtered_ls_publisher_->publish(*msg);
+}
+
+int main(int argc, char * argv[])
+{
+  rclcpp::init(argc, argv);
+  rclcpp::executors::SingleThreadedExecutor exec;
+  rclcpp::NodeOptions options;
+  auto lp_node = std::make_shared<LidarProcessor>(options);
+  exec.add_node(lp_node);
+  exec.spin();
+  rclcpp::shutdown();
   return 0;
 }
